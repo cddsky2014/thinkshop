8c1248579e56873acf72d508f6cfc424f5f941d0
cddsky <cddsky@qq.com>
2016-04-17 12:16:10 +0800

    add new

--- home.php
+++ home.php
@@ -4,9 +4,28 @@ include("./link.php");
 
 $ctg1 = mysql_query("select * from ctg where pid=0");//分类
 
+/*
+if(isset($_GET["cid"])==true){
+	$cid = $_GET["cid"];
+	$sql = "select * from product where p_cid=$cid";
+}else{
+	$sql = "select * from product";
+}*/
 
-$sql = "select * from product";
+
+
+$p=$_GET["p"];
+$size = 2;
+$start = ($p-1)*$size;
+$sql = "select * from product limit $start,$size";// 1-1 *3 起始位置 = 页码-1 * 每页显示条数
+
+
+$total_rows = mysql_num_rows(mysql_query("select * from product"));//总行数
+$pages = ceil($total_rows/$size);//总页数
+
 $p_list = mysql_query($sql);
+
+
 
 
 
@@ -23,7 +42,7 @@ $p_list = mysql_query($sql);
 						$ctg2 = mysql_query("select * from ctg where pid=$pid");
 						while($row2 = mysql_fetch_assoc($ctg2)){
 						?>
-						<li><a href=""><?php echo $row2["cname"];?></a></li>
+						<li><a href="./home.php?cid=<?php echo $row2["cid"];?>"><?php echo $row2["cname"];?></a></li>
 						<?php }?>
 					</ul>
 				</li>
@@ -52,13 +71,14 @@ $p_list = mysql_query($sql);
 		<!--分页-->
 		<div class="page">
 			<ul>
-				<li><a href="">&lt;&lt;</a></li>
-				<li><a href="">&lt;</a></li>
-				<li><a href="">1</a></li>
-				<li><a href="">2</a></li>
-				<li><a href="">3</a></li>
-				<li><a href="">&gt;</a></li>
-				<li><a href="">&gt;&gt;</a></li>
+				<li><a href="?p=1">&lt;&lt;</a></li>
+				<li><a href="?p=<?php echo $p-1;?>">&lt;</a></li>
+				
+				<?php for($i=1;$i<=$pages;$i++){?>
+				<li><a href="?p=<?php echo $i;?>"><?php echo $i;?></a></li>
+				<?php }?>
+				<li><a href="?p=<?php echo $p+1;?>">&gt;</a></li>
+				<li><a href="?p=<?php echo $pages;?>">&gt;&gt;</a></li>
 			</ul>
 		</div>
 		
